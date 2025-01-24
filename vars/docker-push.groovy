5ce32410511a795aea98d0cc2b422698b3f4ca02
pritam4545 <phadtarepritam43@gmail.com>
2025-01-24 13:11:01 +0530

    Update docker-push.groovy

--- vars/docker-push.groovy
+++ vars/docker-push.groovy
@@ -1,10 +1,14 @@
 def call(String tag) {
-  withcredentialsId([usernamePassword(
+  withCredentials([usernamePassword(
     credentialsId: "docker-cred",
     passwordVariable: "docker_pass",
     usernameVariable: "docker_user"
     )]) {
+       echo "Logging into dockerhub with the username ${docker-user}"
        sh "docker login -u ${docker_user} -p ${docker_pass}"
+       echo "Login Successfull in ${docker_user}"
+       echo "Pushing the image into ${docker_user} account"
        sh "docker push ${docker_user}/notes-app:${tag}"
+       echo "Push Successfull into ${docker_user}"
     }
 }
