95e6c32fb05ed163f4c8d04436664d6bebb3cdeb
pritam4545 <phadtarepritam43@gmail.com>
2025-01-24 12:40:24 +0530

    Update buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,4 +1,6 @@
 def call(String tag) {
+       echo "Bulding docker image with tag:${tag}"
        sh "docker build -t pritam44/notes-app:${tag} ."
+       echo "The docker image building is completed with tag:${tag}"
     }
 }
