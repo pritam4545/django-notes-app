d8faa465f5b0eebbab099d3af28ce0fc7e060e71
pritam4545 <phadtarepritam43@gmail.com>
2025-01-24 12:11:45 +0530

    build.groovy typo in cred

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,9 +1,9 @@
 def call(String tag) {
-  withCredeantials([usernamePassword(
+  withcredentialsId([usernamePassword(
     credentialsId: "docker-cred",
     passwordVariable: "docker_pass",
     usernameVariable: "docker_user"
     )]) {
-       sh "docker build -t "${docker_user}"/notes-app:"${tag}""
+       sh "docker build -t ${docker_user}/notes-app:${tag} ."
     }
 }
