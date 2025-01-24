3ee95c269b33b36641925a02705f521805219131
pritam4545 <phadtarepritam43@gmail.com>
2025-01-24 11:57:52 +0530

    build.groovy with cred

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,9 @@
 def call(String tag) {
-  sh "docker build -t "${docker_user}"/notes-app:"${tag}""
+  withCredeantials([usernamePassword(
+    credentialsId: "docker-cred",
+    passwordVariable: "docker_pass",
+    usernameVariable: "docker_user"
+    )]) {
+       sh "docker build -t "${docker_user}"/notes-app:"${tag}""
+    }
 }
