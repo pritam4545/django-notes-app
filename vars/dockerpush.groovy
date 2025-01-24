75cedf0070ff5cfa5e02b86c9095e0a5a6c22295
pritam4545 <phadtarepritam43@gmail.com>
2025-01-24 17:43:44 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,6 +1,6 @@
 def call(String tag) {
   withCredentials([usernamePassword(credentialsId: 'docker-Cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
     sh "docker login -u ${docker_user} -p ${docker_pass}"
+    sh "docker push ${docker_user}/notes-app:${tag}"
   }
-  sh "docker push ${docker_user}/notes-app:${tag}"
 }
