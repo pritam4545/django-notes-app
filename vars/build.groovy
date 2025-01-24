ef865aba354e0b10a54e11008d50b069f6a44094
pritam4545 <phadtarepritam43@gmail.com>
2025-01-24 12:15:51 +0530

    build.groovy exposed username

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,9 +1,4 @@
 def call(String tag) {
-  withcredentialsId([usernamePassword(
-    credentialsId: "docker-cred",
-    passwordVariable: "docker_pass",
-    usernameVariable: "docker_user"
-    )]) {
-       sh "docker build -t ${docker_user}/notes-app:${tag} ."
+       sh "docker build -t pritam44/notes-app:${tag} ."
     }
 }
